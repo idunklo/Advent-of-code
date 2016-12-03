5c07bbe47503415c3a6f4ee65529bf3ab04eb4f5
idunklo <idun.klovstad@gmail.com>
2016-12-03 21:51:26 +0100

    day1 done

--- 2015/day1.py
+++ 2015/day1.py
@@ -1,13 +1,20 @@
 infile = open("input/day1.dat")
 
-floor = 0
+floor = 0  
+pos = 0
 
 for line in infile:
     for par in line:
+        pos +=1
+
         if par == "(":
             floor += 1
         elif par == ")":
             floor -= 1
+
+        if floor == -1:
+            print pos
+
 
 print floor
 
